4c136f31a5470acd16d9978bc3e9c97450cd076a
fashengliu <50940066+fashengliu@users.noreply.github.com>
2021-08-30 08:57:42 +0800

    Upload by PicGo

---
+++ imgabout.php
@@ -0,0 +1,170 @@
+<?php
+require_once 'Head.Include.php';
+if(!$account_check) {
+    echo Tools::href("./?tip_error=请先登陆！");
+}
+?>
+
+
+
+<div class="col-12">
+                                                    <div class="t-pt-120 t-pb-120 px-3 px-md-4 mx-faq-banner">
+                                                        <div class="row">
+                                                            <div class="col-md-6 col-xl-4">
+                                                                <h3 class="mt-0 text-capitalize">
+                                                                    LLQPlayer-Pro
+                                                                </h3>
+                                                                <p>
+                                                                    欢迎使用本H5高级播放器
+                                                                </p>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div
+                                                        class="cards t-bg-white t-shadow"
+                                                    >
+                                                        <div
+                                                            class="cards__header t-pt-30 t-pb-15 t-pl-15 t-pr-15"
+                                                        >
+                                                            <h6
+                                                                class="mt-0 mb-0 text-capitalize"
+                                                            >
+                                                            说明&特色
+                                                            </h6>
+                                                        </div>
+                                                        <div
+                                                            class="cards__body t-pt-15 t-pl-15 t-pr-15 t-pb-30"
+                                                        >
+                                                            <div class="accordion accordion--toggle-icon accordion--primary-bg" id="accThree">
+                                                                <div class="card border-0">
+                                                                  <div class="card-header p-0 border-0">
+                                                                      <div class=" accordion__button sm-text" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="true">
+                                                                        1. 弹出弹窗？广告？
+                                                                      </div>
+                                                                  </div>
+
+                                                                  <div id="collapseSeven" class="collapse show" data-parent="#accThree">
+                                                                    <div class="card-body">
+                                                                    <p class="mb-0 sm-text">
+                                                                      播放器设置->高级模式 被开启，网站流量将给站长带来收入，弹窗为此收入来源的表现。仅部分安卓用户弹出弹窗，不过分影响网站浏览体验。如果不满意将高级模式关闭即可！
+                                                                      
+                                                                      <br/><br/><div class="alert alert-primary" role="alert">
+                                                                <button type="button" class="btn btn-primary" data-toggle="tooltip" data-html="true" title="" data-original-title="<img src='https://s.pc.qq.com/tousu/img/20210627/4690288_1624761307.jpg' width='100%'>">
+                                                                        查看
+                                                                      </button>
+                                                              </div>
+                                                                   
+                                                                    </p>
+                                                                    </div>
+                                                                  </div>
+                                                                </div>
+                                                                <div class="card border-0 t-mt-10">
+                                                                  <div class="card-header p-0 border-0" >
+                                                                      <div class="accordion__button sm-text collapsed" type="button" data-toggle="collapse" data-target="#collapseeight" aria-expanded="false">
+                                                                        2. 余额提现？
+                                                                      </div>
+                                                                  </div>
+                                                                  <div id="collapseeight" class="collapse" data-parent="#accThree">
+                                                                    <div class="card-body">
+                                                                    <p class="mb-0 sm-text">
+                                                                      申请提现后如果提现信息无误，三个工作日内将处理完毕，若信息有误修改信息后重新申请提现即可。单次提现需满 50元。
+                                                                    </p>
+                                                                    </div>
+                                                                  </div>
+                                                                </div>
+                                                                <div class="card border-0 t-mt-10">
+                                                                  <div class="card-header p-0 border-0" >
+                                                                      <div class="accordion__button sm-text collapsed" type="button" data-toggle="collapse" data-target="#collapsenine" aria-expanded="false">
+                                                                        3. 交流群？我有些问题想反馈！
+                                                                      </div>
+                                                                  </div>
+                                                                  <div id="collapsenine" class="collapse" data-parent="#accThree">
+                                                                    <div class="card-body">
+                                                                      <p class="mb-0 sm-text">
+                                                                        官方TG群：<a href="https://t.me/joinchat/vdtA9DBAmD02Mjdl" target="_blank">https://t.me/joinchat/vdtA9DBAmD02Mjdl</a><br/>
+                                                                        反馈邮箱 <font color="blue">loliwlcom@gmail.com</font>
+                                                                      </p>
+                                                                    </div>
+                                                                  </div>
+                                                                </div>
+                                                                
+                                                                <div class="card border-0 t-mt-10">
+                                                                  <div class="card-header p-0 border-0" >
+                                                                      <div class="accordion__button sm-text collapsed" type="button" data-toggle="collapse" data-target="#collapsenine" aria-expanded="false">
+                                                                        4. 特色
+                                                                      </div>
+                                                                  </div>
+                                                                  <div id="collapsenine" class="collapse" data-parent="#accThree">
+                                                                    <div class="card-body">
+                                                                      <p class="mb-0 sm-text">
+                                                                        Bilibili自动对接B站官方弹幕库,B站播放器独家动画 ~ 本人喜欢这个，其余特色就不多说了~
+                                                                        <img src="https://img04.sogoucdn.com/app/a/100540022/2021071801540118568695.png" width="100%">
+                                                                      </p>
+                                                                    </div>
+                                                                  </div>
+                                                                </div>
+                                                                
+                                                                
+                                                                
+                                                                
+                                                                
+                                                              </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div class="px-4 py-4 t-bg-white">
+                                                        <div class="mx-task-complete-container t-mt-30">
+                                                            
+
+                                                            <div class="mx-task-complete align-items-start py-2">
+                                                                <div class="mx-task-complete__date sm-text text-capitalize font-weight-bold">
+                                                                    2021-07-25
+                                                                </div>
+                                                                <div class="mx-task-complete__name">
+                                                                    <span class="d-block sm-text font-weight-bold t-mb-5">
+                                                                        Pro更新至1.1
+                                                                    </span>
+                                                                    <span class="d-block sm-text">
+                                                                        新增离线登陆<br/>
+                                                                        优化部分请求到本地
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+
+                                                            <div class="mx-task-complete align-items-start py-2">
+                                                                <div class="mx-task-complete__date sm-text text-capitalize font-weight-bold">
+                                                                    2021-07-18
+                                                                </div>
+                                                                <div class="mx-task-complete__name">
+                                                                    <span class="d-block sm-text font-weight-bold t-mb-5">
+                                                                        LLQPlayer - Pro
+                                                                    </span>
+                                                                    <span class="d-block sm-text">
+                                                                        H5高级播放器正式上线
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            
+                                                            
+                                                            
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                
+                                    
+     
+
+
+<script>
+var pageName = '关于';
+$("#header-text").text(pageName);
+</script>
+
+<?php
+require_once 'Foot.Include.php';
+?>
+       
